3a1dd72004ca0ed62924e5602db939529ef74ad3
Daksh-Maahor <daksh.dav2007@gmail.com>
2025-04-13 21:54:47 +0530

    Changes

--- main.py
+++ main.py
@@ -273,3 +273,22 @@ class ThreeDimensional(ThreeDScene):
         self.wait(2)
 
         self.stop_ambient_camera_rotation()
+
+class Demo(ThreeDScene):
+    def construct(self):
+        self.set_camera_orientation(phi=60 * DEGREES, theta=-45 * DEGREES)
+
+        axes = ThreeDAxes()
+
+        graph = axes.plot(lambda x : x**2, x_range=[-2, 2], color=YELLOW)
+        surface = Surface(lambda u, v : axes.c2p(v * np.cos(u), v * np.sin(u), 0.5 * v**2),
+                          u_range=[0, 2*PI],
+                          v_range=[0, 3],
+                          checkerboard_colors=[GREEN, RED])
+        
+        three_d_stuff = VGroup(axes, graph, surface)
+
+        self.add(axes, graph)
+        self.begin_ambient_camera_rotation(rate=PI/20)
+        self.play(Create(surface))
+        self.play(three_d_stuff.animate.shift(LEFT * 5))
