19091b17f948e1e3bb5fedce1a5d6074b282fc74
Daksh-Maahor <daksh.dav2007@gmail.com>
2025-04-13 15:13:38 +0530

    graphing fixed

--- main.py
+++ main.py
@@ -80,17 +80,22 @@ class GraphingMovement(Scene):
         self.play(axes.animate.shift(LEFT * 3), run_time=5)
 
 class Graphing(Scene):
+    def f(self, x):
+        return 0.1 * x * (x-5) * (x+5)
+
     def construct(self):
         my_plane = NumberPlane(x_range=[-6, 6], x_length=5, y_range=[-10, 10], y_length=5)
         my_plane.add_coordinates()
         my_plane.shift(RIGHT * 3)
 
-        my_function = my_plane.plot(lambda x : 0.1 * x * (x-5) * (x+5), x_range=[-6, 6], color=GREEN_B)
+        my_function = my_plane.plot(lambda x : self.f(x), x_range=[-6, 6], color=GREEN_B)
         area = my_plane.get_area(graph=my_function, x_range=[-5, 5], color=[BLUE, YELLOW])
 
         label = MathTex("f(x) = 0.1x(x-5)(x+5)").next_to(my_plane, UP, buff=0.2)
 
-        horiz_line = Line(start=my_plane.c2p(0, my_function.function(-2)), end=my_plane.c2p(-2, my_function.function(-2)), stroke_color=YELLOW, stroke_width=10)
+        y_val = self.f(-2)
+
+        horiz_line = Line(start=my_plane.c2p(0, y_val), end=my_plane.c2p(-2, y_val), stroke_color=YELLOW, stroke_width=10)
 
         self.play(DrawBorderThenFill(my_plane))
         self.play(Create(my_function), Write(label))
