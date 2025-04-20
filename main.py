1bc8f181ef2383e8fa5ac5aabde801d3423a5bd7
Daksh Maahor <daksh.dav2007@gmail.com>
2025-04-20 22:43:03 +0530

    Hehe

--- main.py
+++ main.py
@@ -700,3 +700,138 @@ class FourierSeriesIllustration(Scene):
             self.generate_nth_func(n)(x)
             for n in self.n_range[:k]
         ])
+    
+class AdditiveFunctions(Scene):
+    def construct(self):
+        axes = (
+            Axes(
+                x_range=[0, 2.1, 1],
+                x_length=12,
+                y_range=[0, 7, 2],
+                y_length=7
+            )
+            .add_coordinates()
+            .to_edge(DL, buff=0.25)
+        )
+
+        func1 = axes.plot(lambda x: x**2, x_range=[0, 2], color=YELLOW)
+        func1_label = (
+            MathTex(r"y={x}^{2}")
+            .scale(0.8)
+            .next_to(func1, UR, buff=0)
+            .set_color(YELLOW)
+        )
+
+        func2 = axes.plot(lambda x: x, x_range=[0, 2], color=GREEN)
+        func2_label = (
+            MathTex(r"y=x")
+            .scale(0.8)
+            .next_to(func2, UR, buff=0)
+            .set_color(GREEN)
+        )
+
+        func3 = axes.plot(lambda x: x**2 + x, x_range=[0, 2], color=PURPLE_D)
+        func3_label = (
+            MathTex(r"y={x}^{2} + x")
+            .scale(0.8)
+            .next_to(func3, UR, buff=0)
+            .set_color(PURPLE_D)
+        )
+
+        self.add(
+            axes, func1, func2, func3,
+            func1_label, func2_label, func3_label
+        )
+        self.wait()
+
+        for k in np.arange(0.2, 2.1, 0.2):
+            line1 = DashedLine(
+                start=axes.c2p(k, 0),
+                end=axes.c2p(k, func1.underlying_function(k)),
+                stroke_color=YELLOW,
+                stroke_width=5
+            )
+
+            line2 = DashedLine(
+                start=axes.c2p(k, 0),
+                end=axes.c2p(k, func2.underlying_function(k)),
+                stroke_color=GREEN,
+                stroke_width=7
+            )
+
+            line3 = DashedLine(
+                start=axes.c2p(k, 0),
+                end=axes.c2p(k, func3.underlying_function(k)),
+                stroke_color=PURPLE,
+                stroke_width=10
+            )
+
+            self.play(Create(line1), run_time=0.5)
+            self.play(Create(line2), run_time=0.5)
+
+            if(len(line1) > len(line2)):
+                self.play(line2.animate.shift(UP*line1.get_length()), run_time=1)
+            else:
+                self.play(line1.animate.shift(UP*line2.get_length()), run_time=1)
+
+            self.play(Create(line3), run_time=0.5)
+        self.wait()
+
+        # areas
+        area1 = axes.get_riemann_rectangles(
+            graph=func1,
+            x_range=[0, 2],
+            dx=0.1,
+            color=[BLUE, GREEN]
+        )
+
+        area2 = axes.get_riemann_rectangles(
+            graph=func2,
+            x_range=[0, 2],
+            dx=0.1,
+            color=[YELLOW, PURPLE]
+        )
+
+        self.play(Create(area1), run_time=5)
+        self.play(area1.animate.set_opacity(0.5))
+        self.play(Create(area2), run_time=5)
+        self.wait()
+        for k in range(20):
+            self.play(area2[k].animate.shift(UP*area1[k].height), run_time=0.25)
+        self.wait()
+
+class IntegralThroughReimannRectangles(Scene):
+    def construct(self):
+        # Initialize the ValueTracker for dx
+        dx = ValueTracker(0.5)
+
+        # Create the NumberPlane
+        plane = NumberPlane(x_range=[-9, 9, 1], y_range=[-10, 10, 2], x_length=14, y_length=10)
+        plane.add_coordinates()
+
+        # Define the graph
+        graph = plane.plot(
+            lambda x: 0.00349632 * x**3 + 0.0280286 * x**2 - 0.660808 * x - 1.70342,
+            x_range=[-8, 8],
+            color=YELLOW
+        )
+
+        # Define the Riemann rectangles
+        area = always_redraw(lambda: plane.get_riemann_rectangles(
+            graph=graph,
+            x_range=[-8, 8],
+            dx=dx.get_value(),
+            fill_opacity=0.5
+        ))
+
+        # Add the plane and graph to the scene
+        self.play(Write(plane), Create(graph), run_time=2)
+        self.play(Create(area), run_time=2)
+
+        # Periodically decrease the step size (dx)
+        step_sizes = [0.4, 0.3, 0.2, 0.1]  # Define the step sizes
+        for step in step_sizes:
+            self.play(dx.animate.set_value(step), run_time=0.5, rate_func=linear)
+
+        # Pause to display the final result
+        self.wait()
